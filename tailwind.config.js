34ee98518e4f73d80ed9ed2da4d8c22339aa02bb
gulcinozpmk <gulcinozpamuk@hotmail.com>
2026-03-10 18:25:20 +0300

    Tailwind padding update

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,12 +8,6 @@ export default {
     container: {
       center: true,
       padding: '1.5rem',
-      screens: {
-        sm: '640px',
-        md: '768px',
-        lg: '1024px',
-        xl: '1280px',        
-      },
     },
     extend: {
       colors: {
