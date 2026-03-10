fb07deaa7690d1f7d37e70b8cd6f53725d097451
gulcinozpmk <gulcinozpamuk@hotmail.com>
2026-03-10 18:19:07 +0300

    Tailwind update

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,8 +12,7 @@ export default {
         sm: '640px',
         md: '768px',
         lg: '1024px',
-        xl: '1280px',
-        '2xl': '1280px',
+        xl: '1280px',        
       },
     },
     extend: {
